0f4530b17badc133960f70c9c3cd35cffce06843
rakesh yadav <rakesh900571@gmai.com>
2026-08-15 20:32:03 +0530

    update

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -6,8 +6,15 @@ terraform {
       version = "4.80.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "RG-RAKESH"
+    storage_account_name = "dev9005711880"
+    container_name       = "tfstate"
+    key                  = "prod.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
 }
+
